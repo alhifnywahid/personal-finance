d41c38b00ca6d85a84aabc8ece67b38f8580e0e4
alhifnywahid <hifnycarlytosss@gmail.com>
2025-04-20 15:51:12 +0700

    change favicon

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,6 @@ const urlsToCache = [
   "/icons/icon-512x512.png",
 ]
 
-// Install a service worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -21,11 +20,9 @@ self.addEventListener("install", (event) => {
   )
 })
 
-// Cache and return requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Cache hit - return response
       if (response) {
         return response
       }
@@ -34,7 +31,6 @@ self.addEventListener("fetch", (event) => {
   )
 })
 
-// Update a service worker
 self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME]
   event.waitUntil(
